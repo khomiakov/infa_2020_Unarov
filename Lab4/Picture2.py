23f4244e9f3355afe4bbd37ed084eac50a915232
khomiakov <khomiakov.io@phystech.edu>
2020-09-29 16:23:19 +0300

    picture2khoma.py

--- Lab4/Picture2.py
+++ Lab4/Picture2.py
@@ -26,42 +26,22 @@ pg.draw.rect(screen, color_tree, (int(screen_width * 0.9), 0, 60, 520))
 pg.draw.rect(screen, color_tree, (int(screen_width * 0), 0, 40, 440))
 
 
-def igolki(x, y, hedgehog_width, hedgehog_height):
+def igolki(x, y, hedgehog_width, hedgehog_height,n):
     a = hedgehog_width / 2.3
     b = hedgehog_height / 2.3
 
-    for i in range(30):
+    for i in range(n):
         x0 = random.randint(-int(a), int(a))
         y0 = random.randint(-int(b * (1 - (x0 / a) ** 2) ** 0.5), int(b * (1 - (x0 / a) ** 2) ** 0.5))
-        k = random.uniform(-np.pi / 5, np.pi / 5)
+        k = random.uniform(-np.pi / 4, np.pi / 4)
         A = [screen_width * x + a + x0, screen_height * y + b + y0]
         B = [screen_width * x + a + x0 + 20 * np.cos(k), screen_height * y + b + y0 - 20 * np.sin(k)]
         C = [screen_width * x + a + x0 + 5 - 90 * np.sin(k), screen_height * y + b + y0 - 90 * np.cos(k)]
         pg.draw.polygon(screen, (30, 30, 30), (A, B, C), )
         pg.draw.polygon(screen, (0, 0, 0), [A, B, C], 1)
 
-    for i in range(20):
-        x0 = random.randint(-int(a), int(a))
-        y0 = random.randint(-int(b * (1 - (x0 / a) ** 2) ** 0.5), int(b * (1 - (x0 / a) ** 2) ** 0.5))
-        k = random.uniform(-np.pi / 5, np.pi / 5)
-        A = [screen_width * x + a + x0, screen_height * y + b + y0]
-        B = [screen_width * x + a + x0 + 20 * np.cos(k), screen_height * y + b + y0 - 20 * np.sin(k)]
-        C = [screen_width * x + a + x0 + 5 - 90 * np.sin(k), screen_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(screen, (30, 30, 30), [A, B, C], )
-        pg.draw.polygon(screen, black, [A, B, C], 1)
 
-    for i in range(10):
-        x0 = random.randint(-int(a), int(a))
-        y0 = random.randint(-int(b * (1 - (x0 / a) ** 2) ** 0.5), int(b * (1 - (x0 / a) ** 2) ** 0.5))
-        k = random.uniform(-np.pi / 5, np.pi / 5)
-        A = [screen_width * x + a + x0, screen_height * y + b + y0]
-        B = [screen_width * x + a + x0 + 20 * np.cos(k), screen_height * y + b + y0 - 20 * np.sin(k)]
-        C = [screen_width * x + a + x0 + 5 - 90 * np.sin(k), screen_height * y + b + y0 - 90 * np.cos(k)]
-        pg.draw.polygon(screen, (30, 30, 30), [A, B, C], )
-        pg.draw.polygon(screen, (0, 0, 0), [A, B, C], 1)
-
-
-def mashroom(x, y):
+def mushroom(x, y):
     pg.draw.ellipse(screen, white,
                     (screen_width * x + hedgehog_width - 140, screen_height * y + hedgehog_height - 160, 20, 70))
     pg.draw.ellipse(screen, (255, 127, 116),
@@ -103,7 +83,9 @@ def hedgehog(x, y):
     pg.draw.ellipse(screen, (230, 145, 0),
                     (screen_width * x + hedgehog_width - 200, screen_height * y + hedgehog_height - 140, 50, 50))
 
-    igolki(x, y, hedgehog_width, hedgehog_height)
+    igolki(x, y, hedgehog_width, hedgehog_height,50)
+    mushroom(x, y)
+    igolki(x, y, hedgehog_width, hedgehog_height, 10)
 
 
 hedgehog(0.6, 0.7)
